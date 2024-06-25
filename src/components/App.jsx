dec71904462984f4e5d00ea6ac35bdd910276e8a
devromarc <rmrcslvdr@gmail.com>
2024-06-26 01:57:57 +0800

    Done with the functionality. CSS not yet added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
 import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
 
 export class App extends Component {
   state = {
@@ -8,101 +11,57 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    name: '',
-    number: '',
-    search: '',
+    filter: '',
   };
 
-  handleNameChange = e => {
-    this.setState({
-      name: e.target.value,
-    });
-    // console.log(this.state.name);
-  };
-
-  handleNumberChange = e => {
-    this.setState({
-      number: e.target.value,
-    });
-    // console.log(this.state.name);
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
+  // Get the New contact from ContactForm and store it in State
+  addNewContact = newContact => {
     this.setState(prevState => ({
-      contacts: [
-        ...prevState.contacts,
-        { name: prevState.name, number: prevState.number },
-      ],
-      name: '',
-      number: '',
+      contacts: [...prevState.contacts, newContact],
     }));
   };
 
-  handleSearchChange = e => {
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  handleFilter = e => {
     this.setState({
-      search: e.target.value,
+      filter: e.target.value,
     });
   };
 
-  render() {
-    const { contacts, name, number, search } = this.state;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(search.toLowerCase())
+  setFilter = filterValue => {
+    this.setState({
+      filter: filterValue,
+    });
+  };
+
+  filterContact = () => {
+    const { contacts, filter } = this.state;
+    const filterLowerCase = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterLowerCase)
     );
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
     return (
       <>
-        <div>
-          <h2>Phonebook</h2>
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              <p>Name</p>
-              <input
-                type="text"
-                name="name"
-                // add \ before - in [' \-] to make it work (LMS)
-                pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
-                required
-                value={name}
-                onChange={this.handleNameChange}
-              />
-            </label>
-            <label>
-              <p>Telephone</p>
-              <input
-                type="tel"
-                name="number"
-                pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-                value={number}
-                onChange={this.handleNumberChange}
-              />
-            </label>
-            <button type="submit">Submit</button>
-          </form>
-        </div>
+        <h1>Phonebook</h1>
+        <ContactForm addNewContact={this.addNewContact} contacts={contacts} />
 
         <div>
           <h2>Contacts</h2>
-          <label>
-            <p>Search Contacts</p>
-            <input
-              type="text"
-              value={search}
-              onChange={this.handleSearchChange}
-            />
-          </label>
-          <ul>
-            {filteredContacts.map((contact, index) => (
-              <li key={index}>
-                <p>
-                  {contact.name}: {contact.number}
-                </p>
-              </li>
-            ))}
-          </ul>
+          <Filter filter={filter} setFilter={this.setFilter} />
+          <ContactList
+            filterContact={this.filterContact}
+            deleteContact={this.deleteContact}
+          />
         </div>
       </>
     );
---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+export const ContactListItem = ({ filteredContact, deleteContact }) => {
+  // handleDelete method
+  const handleDelete = () => {
+    deleteContact(filteredContact.id);
+  };
+
+  return (
+    <li>
+      <p>{filteredContact.name}:</p>
+      <p>{filteredContact.number}</p>
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
+};
+
+ContactListItem.propTypes = {
+  filteredContact: PropTypes.object.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
